49f448c6419ef061ee9199b86c2d359327f5813a
Alaska-999 <kristinagaranchuk@gmail.com>
2022-06-06 21:06:23 +0300

    connected to redux and used it to create button count

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+//тут стор и корневой редьюсер
+
+//combine reducer обьеденяет все редьюсеры которые хранятся в приложении
+import {combineReducers} from "redux";
+//apply middleware подключает redux thunk
+import {createStore, applyMiddleware} from "redux";
+import reposReducer from "./reposReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+    repos:reposReducer
+})
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
